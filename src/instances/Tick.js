70719842bb3a1a8fad3971c4784011169bddcde7
nuancednovice <88918386+nuancednovice@users.noreply.github.com>
2022-09-24 13:12:10 -0700

    Update Tick.js

--- src/instances/Tick.js
+++ src/instances/Tick.js
@@ -3,7 +3,7 @@ import TickEntity from 'lib/entities/TickEntity';
 export const initialTickState = {
   interval: null,
   pointer: 0,
-  target: 300000,
+  target: 600000,
 };
 
 const Tick = new TickEntity({ ...initialTickState });
